e897c7410351dc57c9b5601e0bb2448839ae559a
Tayyaba <tayyaba.tahir@hotmail.co.uk>
2024-04-07 00:40:11 +0100

    database_utils.py

--- database_utils.py
+++ database_utils.py
@@ -2,15 +2,31 @@ import yaml
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 from urllib.parse import quote
-from data_cleaning import DataCleaning
-from data_extraction import DataExtractor
+
 
 if __name__ == "__main__":
+    from data_extraction import DataExtractor
+    from data_cleaning import DataCleaning
     extractor = DataExtractor("/Users/tayya/multinational-retail-data-centralisation246\db_creds.yaml")
+    table_name1 = 'legacy_store_details'
     table_name2 = 'legacy_users'
+    df_legacy_store_details = extractor.read_rds_table(table_name1)
     df_legacy_users = extractor.read_rds_table(table_name2)
-if __name__ == "__main__":
     cleaned_user_data = DataCleaning.clean_user_data(df_legacy_users)
+    cleaned_store_data = DataCleaning.clean_store_data(df_legacy_store_details)
+    card_data =  extractor.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+    cleaned_card_data = DataCleaning.clean_card_data(card_data)
+    s3_url = 's3://data-handling-public/products.csv'
+    df_products_data = DataExtractor.extract_from_s3(s3_url)
+    converted_products_weights_data = DataCleaning.convert_product_weights(df_products_data)
+    cleaned_products_data = DataCleaning.clean_products_data(converted_products_weights_data)
+    table_name3 = 'orders_table'
+    df_orders_table = extractor.read_rds_table(table_name3)
+    cleaned_orders_data = DataCleaning.clean_orders_data(df_orders_table)
+    s3_url_of_date_time_data = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/date_details.json'
+    df_date_time_data = DataExtractor.extract_from_s3_jason(s3_url_of_date_time_data)
+    cleaned_date_time_data = DataCleaning.clean_date_time_data(df_date_time_data)
+    
 
 class DatabaseConnector:
 
@@ -107,5 +123,17 @@ if __name__ == "__main__":
     db_name = 'sales_data'
 
     connection_string = f"postgresql+psycopg2://{db_user}:{encoded_password}@{db_host}:{db_port}/{db_name}"
-    table_name = 'dim_users'
-    DatabaseConnector.upload_to_db(connection_string, cleaned_user_data, table_name)
+    table_name_1 = 'dim_users'
+    table_name_2 = 'dim_card_details'
+    table_name_3 = 'dim_store_details'
+    table_name_4 = 'dim_products'
+    table_name_5 = 'orders_table'
+    table_name_6 = 'dim_date_times'
+
+    #I have put the following in hash so that python doesn't keep on replacing the tables.
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_user_data, table_name_1)
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_card_data, table_name_2)
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_store_data, table_name_3)
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_products_data, table_name_4)
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_orders_data, table_name_5)
+    #DatabaseConnector.upload_to_db(connection_string, cleaned_date_time_data, table_name_6)
